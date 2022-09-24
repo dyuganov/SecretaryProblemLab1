41ffa6c14f0faeae4ed92bc53e5306f82d4c44ca
Nikita <dyuganov.n@gmail.com>
2022-09-24 09:40:34 +0700

    done

--- SecretaryProblem1/Friend.cs
+++ SecretaryProblem1/Friend.cs
@@ -2,5 +2,21 @@
 
 public class Friend
 {
-    
+    //private readonly List<Contender> _rejectedContenders = new List<Contender>();
+    private int _maxValue = Int32.MinValue;
+
+    public void RememberContender(Contender contender)
+    {
+        var currentContenderValue = contender.GetValue();
+        if (_maxValue < currentContenderValue)
+        {
+            _maxValue = currentContenderValue;
+        }
+        //_rejectedContenders.Add(contender);
+    }
+
+    public bool IsBetterThanOthers(Contender contender)
+    {
+        return contender.GetValue() > _maxValue;
+    }
 }
--- SecretaryProblem1/Princess.cs
+++ SecretaryProblem1/Princess.cs
@@ -2,18 +2,28 @@
 
 public class Princess
 {
-    private Friend _friend;
+    private readonly Friend _friend;
     private uint _skippedContendersCnt = 0;
+    private const int ContendersToSkip = 36;
 
     public Princess(Friend friend)
     {
         _friend = friend;
     }
-
-    // true == marry this contender, false == skip
+    
+    // null == skip, contender == choose this 
     public Contender? MakeChoice(Contender contender)
     {
-        
+        if (_skippedContendersCnt < ContendersToSkip)
+        {
+            _friend.RememberContender(contender);
+            ++_skippedContendersCnt;
+            return null;
+        }
+
+        if (_friend.IsBetterThanOthers(contender)) return contender;
+        _friend.RememberContender(contender);
+        ++_skippedContendersCnt;
         return null;
     }
 }
